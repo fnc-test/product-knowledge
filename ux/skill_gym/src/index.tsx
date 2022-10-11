ce0f1dc556405ab1aa2b5cfff827a8183719865e
Janine Semmler <contact@janemcbrain.com>
2022-10-11 15:46:07 +0200

    add shared components and add Navigation

--- ux/skill_gym/src/index.tsx
+++ ux/skill_gym/src/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import CustomSearch from './pages/CustomSearch';
+import SkillGym from './pages/SkillGym';
 import App from './App';
+import {
+  SharedThemeProvider,
+  SharedCssBaseline,
+} from 'cx-portal-shared-components'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SharedCssBaseline />
+    <SharedThemeProvider>
+      <Router>
+        <Routes>
+          <Route element={<App />}>
+            <Route path="/" element={<Navigate replace to="/skill-gym" />}/>
+            <Route path="/skill-gym" element={<SkillGym />} />
+            <Route path="/custom-search" element={<CustomSearch />} />
+          </Route>
+        </Routes>
+      </Router>
+    </SharedThemeProvider>
   </React.StrictMode>,
   document.getElementById('app')
 );
---
+++ ux/skill_gym/src/pages/CustomSearch.tsx
@@ -0,0 +1,5 @@
+export default function CustomSearch(){
+  return(
+    <h1>Hello Custom Search</h1>
+  )
+}
