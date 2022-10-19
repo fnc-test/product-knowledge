68828b6737b962f39e4c71a1799aec0854b5844a
Janine Semmler <contact@janemcbrain.com>
2022-10-19 12:53:59 +0200

    remove unused index.ts

--- ux/src/index.ts
+++
@@ -1,9 +0,0 @@
-//
-// Main entry into the UX server
-// See copyright notice in the top folder
-// See authors file in the top folder
-// See license file in the top folder
-//
-
-// issue a module loading message
-console.log("Debug: Loading ux/index");
