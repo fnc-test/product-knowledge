9c82210262aa8226aa6759503b508c4bcb6dc0dc
Janine Semmler <janine.semmler@t-systems.com>
2022-12-12 15:07:11 +0100

    Change/add tooltip to datalist (#55)
    * add tooltip to datalist
    
    * https://jira.catena-x.net/browse/KA-121 linting stuff plus escaping of values and right-justified ellipsis
    
    Co-authored-by: Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>

--- ux/skill_modules/src/components/DataList/index.tsx
+++ ux/skill_modules/src/components/DataList/index.tsx
@@ -1,152 +1,41 @@
 import { BindingSet, Entry } from '@catenax-ng/skill-framework/dist/src';
 import { Table, Typography } from 'cx-portal-shared-components';
 import React from 'react';
-import {
-  GridColDef,
-  GridRowId,
-  GridRowModel,
-  GridRenderCellParams,
-} from '@mui/x-data-grid';
-import { Box } from '@mui/material';
+import { GridColDef, GridRowId, GridRowModel } from '@mui/x-data-grid';
+import { Box, Tooltip } from '@mui/material';
 import ErrorTwoToneIcon from '@mui/icons-material/ErrorTwoTone';
-import Paper from '@mui/material/Paper';
-import Popper from '@mui/material/Popper';
 
-interface GridCellExpandProps {
-  value: string;
-  width: number;
-}
-
-function isOverflown(element: Element): boolean {
-  return (
-    element.scrollHeight > element.clientHeight ||
-    element.scrollWidth > element.clientWidth
-  );
-}
-
-const GridCellExpand = React.memo(function GridCellExpand(
-  props: GridCellExpandProps
-) {
-  const { width, value } = props;
-  const wrapper = React.useRef<HTMLDivElement | null>(null);
-  const cellDiv = React.useRef(null);
-  const cellValue = React.useRef(null);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [showFullCell, setShowFullCell] = React.useState(false);
-  const [showPopper, setShowPopper] = React.useState(false);
-
-  const handleMouseEnter = () => {
-    const isCurrentlyOverflown = isOverflown(cellValue.current!);
-    setShowPopper(isCurrentlyOverflown);
-    setAnchorEl(cellDiv.current);
-    setShowFullCell(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowFullCell(false);
-  };
-
-  React.useEffect(() => {
-    if (!showFullCell) {
-      return undefined;
-    }
-
-    function handleKeyDown(nativeEvent: KeyboardEvent) {
-      // IE11, Edge (prior to using Bink?) use 'Esc'
-      if (nativeEvent.key === 'Escape' || nativeEvent.key === 'Esc') {
-        setShowFullCell(false);
-      }
-    }
-
-    document.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [setShowFullCell, showFullCell]);
-
-  return (
-    <Box
-      ref={wrapper}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      sx={{
-        alignItems: 'center',
-        lineHeight: '24px',
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-        display: 'flex',
-      }}
-    >
-      <Box
-        ref={cellDiv}
-        sx={{
-          height: '100%',
-          width,
-          display: 'block',
-          position: 'absolute',
-          top: 0,
-        }}
-      />
-      <Box
-        ref={cellValue}
-        sx={{
-          whiteSpace: 'nowrap',
-          overflow: 'hidden',
-          direction: 'rtl',
-          textOverflow: 'ellipsis',
-        }}
-      >
-        {value}
-      </Box>
-      {showPopper && (
-        <Popper
-          open={showFullCell && anchorEl !== null}
-          anchorEl={anchorEl}
-          style={{ width, marginLeft: -17 }}
-        >
-          <Paper
-            elevation={1}
-            style={{ minHeight: wrapper.current!.offsetHeight - 3 }}
-          >
-            <Typography variant="body2" style={{ padding: 8 }}>
-              {value}
-            </Typography>
-          </Paper>
-        </Popper>
-      )}
-    </Box>
-  );
-});
-
-const renderCellExpand = (params: GridRenderCellParams<string>) => {
-  return (
-    <GridCellExpand
-      value={params.value || ''}
-      width={params.colDef.computedWidth}
-    />
-  );
-};
-
-export const DataList = ({
-  search,
-  id,
-  data,
-}: {
+interface DataListProps {
   search: string;
   id: string;
   data: BindingSet;
-}) => {
+}
+
+export const DataList = ({ search, id, data }: DataListProps) => {
   const tableTitle = `Results for ${search}`;
   const resultToColumns = (result: string[]): Array<GridColDef> =>
     result.map((item, index) => ({
       field: item,
       flex: 2,
+      renderCell: ({ row }: { row: Entry }) => {
+        let val = row[item] ? row[item].value : '';
+        val = val.replace('\\"', '"').replace('\\n', '\n');
+        return (
+          <Tooltip title={val}>
+            <span
+              style={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                direction: 'rtl',
+                textOverflow: 'ellipsis',
+              }}
+            >
+              {val}
+            </span>
+          </Tooltip>
+        );
+      },
       hide: index > 5,
-      renderCell: renderCellExpand,
-      valueGetter: ({ row }: { row: Entry }) =>
-        row[item].value ? row[item].value : 'No Value',
     }));
 
   const rowId = (row: GridRowModel): GridRowId => {
