8cc65c6438556a7b6882ee833adfcc6f97714585
obalandi <36449118+obalandi@users.noreply.github.com>
2023-01-12 14:47:15 +0100

    Quality skill is implemented. Includes data, mapping and query. (#60)
    * Quality skill is implemented. Includes data, mapping and query.
    
    * https://jira.catena-x.net/browse/KA-50 deploy quality graph/endpoint
    
    * SQLs for dremio quality views
    
    Co-authored-by: Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>

--- dataspace/virtualize/dremio/resources/dremio.sql
+++ dataspace/virtualize/dremio/resources/dremio.sql
@@ -55,3 +55,31 @@ FROM (
     FROM "Local"."dtc_codes.json"
   ) contents
 ) parts
+
+/** Quality Skill */
+
+/** data from cloud **/
+/**
+CREATE VIEW "quality"."DTC" AS SELECT * FROM datalake."catenax-knowledge-agents"."DTC.xlsx"
+CREATE VIEW "quality"."DTC_PossibleCause_Error" AS SELECT * FROM datalake."catenax-knowledge-agents"."DTC_PossibleCause_Error.xlsx"
+CREATE VIEW "quality"."Error" AS SELECT * FROM datalake."catenax-knowledge-agents"."Error.xlsx"
+CREATE VIEW "quality"."Error_ErrorEffect" AS SELECT * FROM datalake."catenax-knowledge-agents"."Error_ErrorEffect.xlsx"
+CREATE VIEW "quality"."Error_ErrorSolution" AS SELECT * FROM datalake."catenax-knowledge-agents"."Error_ErrorSolution.xlsx"
+CREATE VIEW "quality"."ErrorEffect" AS SELECT * FROM datalake."catenax-knowledge-agents"."ErrorEffect.xlsx"
+CREATE VIEW "quality"."ErrorSolution" AS SELECT * FROM datalake."catenax-knowledge-agents"."ErrorSolution.xlsx"
+CREATE VIEW "quality"."Part" AS SELECT * FROM datalake."catenax-knowledge-agents"."Part.xlsx"
+CREATE VIEW "quality"."Part_PossibleCause" AS SELECT * FROM datalake."catenax-knowledge-agents"."Part_PossibleCause.xlsx"
+CREATE VIEW "quality"."PossibleCause" AS SELECT * FROM datalake."catenax-knowledge-agents"."PossibleCause.xlsx"
+**/
+
+/** data from local system **/
+CREATE VIEW "quality"."DTC" AS SELECT * FROM "local"."DTC.xlsx"
+CREATE VIEW "quality"."DTC_PossibleCause_Error" AS SELECT * FROM "local"."DTC_PossibleCause_Error.xlsx"
+CREATE VIEW "quality"."Error" AS SELECT * FROM "local"."Error.xlsx"
+CREATE VIEW "quality"."Error_ErrorEffect" AS SELECT * FROM "local"."Error_ErrorEffect.xlsx"
+CREATE VIEW "quality"."Error_ErrorSolution" AS SELECT * FROM "local"."Error_ErrorSolution.xlsx"
+CREATE VIEW "quality"."ErrorEffect" AS SELECT * FROM "local"."ErrorEffect.xlsx"
+CREATE VIEW "quality"."ErrorSolution" AS SELECT * FROM "local"."ErrorSolution.xlsx"
+CREATE VIEW "quality"."Part" AS SELECT * FROM "local"."Part.xlsx"
+CREATE VIEW "quality"."Part_PossibleCause" AS SELECT * FROM "local"."Part_PossibleCause.xlsx"
+CREATE VIEW "quality"."PossibleCause" AS SELECT * FROM "local"."PossibleCause.xlsx"
