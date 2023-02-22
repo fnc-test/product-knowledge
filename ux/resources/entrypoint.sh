62202ce406f4165e6a3de1fd934d2113a8ac2029
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2023-02-22 09:17:11 +0100

    PI7 Release - SKILL_BACKEND environment was lacking

--- ux/resources/entrypoint.sh
+++ ux/resources/entrypoint.sh
@@ -10,11 +10,12 @@
 
 for file in /usr/local/tomcat/webapps/ROOT/static/js/*.js
 do
-  sed -i -e "s#\"REACT_APP_SKILL_CONNECTOR_CONTROL\"#\"${REACT_APP_SKILL_CONNECTOR_CONTROL//#/\\#}\"#g" \
+  sed -i '' -e "s#\"REACT_APP_SKILL_CONNECTOR_CONTROL\"#\"${REACT_APP_SKILL_CONNECTOR_CONTROL//#/\\#}\"#g" \
       -e "s#\"REACT_APP_SKILL_CONNECTOR_DATA\"#\"${REACT_APP_SKILL_CONNECTOR_DATA//#/\\#}\"#g" \
       -e "s#\"REACT_APP_SKILL_CONNECTOR_AUTH_HEADER_KEY\"#\"${REACT_APP_SKILL_CONNECTOR_AUTH_HEADER_KEY//#/\\#}\"#g" \
       -e "s#\"REACT_APP_SKILL_CONNECTOR_AUTH_HEADER_VALUE\"#\"${REACT_APP_SKILL_CONNECTOR_AUTH_HEADER_VALUE//#/\\#}\"#g" \
       -e "s#\"REACT_APP_SKILL_GITHUB_ONTOLOGYHUB\"#\"${REACT_APP_SKILL_GITHUB_ONTOLOGYHUB//#/\\#}\"#g" \
+      -e "s#\"REACT_APP_SKILL_BACKEND\"#\"${REACT_APP_SKILL_BACKEND//#/\\#}\"#g" \
       -e "s#\"REACT_APP_FOLDER\"#\"${REACT_APP_FOLDER//#/\\#}\"#g" $file
 done
 
